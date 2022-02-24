4bedab61a28510beaa232720f585779cbf9e1943
BerkayMertt <berkay_hakan_gs@hotmail.com>
2022-02-24 22:51:16 +0300

    * İnternet yetkisi verildi. * Tasarım düzenlemesi yapıldı. * Adapter düzenlendi ve veriyiGetir Fonksiyonu ile veriler getirildi. * adapter recycle view bağlantısı yapıldı. * Dinamik String xml kullanıldı.

--- app/src/main/java/com/book/bookmarket/MainActivity.kt
+++ app/src/main/java/com/book/bookmarket/MainActivity.kt
@@ -2,35 +2,34 @@ package com.book.bookmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.book.bookmarket.adapter.BookListAdapter
 import com.book.bookmarket.databinding.ActivityMainBinding
 import com.book.bookmarket.model.Book
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private var booklist : ArrayList<Book> = arrayListOf()
+    private var adapter = BookListAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        booklist.add(Book("Hayvan Çiftliği",25,"https://i.dr.com.tr/cache/600x600-0/originals/0000000105409-1.jpg"))
+        booklist.add(Book("1984",34,"https://i.dr.com.tr/cache/600x600-0/originals/0000000064038-1.jpg"))
+        booklist.add(Book("Cesur Yeni Dünya",34,"https://i.dr.com.tr/cache/600x600-0/originals/0000000066424-1.jpg"))
+        booklist.add(Book("50 Soruda Evrim",32,"https://i.dr.com.tr/cache/500x400-0/originals/0001896416001-1.jpg"))
+        booklist.add(Book("Tanzimat",56,"https://m.media-amazon.com/images/I/51Uv3M1JWeL._AC_SY1000_.jpg"))
+        booklist.add(Book("Maymunlar Gezegeni",28,"https://img.kitapyurdu.com/v1/getImage/fn:11240119/wh:true/miw:200/mih:200"))
 
-        val kitap1 = Book("Hayvan Çiftliği",25,"https://i.dr.com.tr/cache/600x600-0/originals/0000000105409-1.jpg")
-        val kitap2 = Book("1984",34,"https://i.dr.com.tr/cache/600x600-0/originals/0000000064038-1.jpg")
-        val kitap3 = Book("Cesur Yeni Dünya",34,"https://i.dr.com.tr/cache/600x600-0/originals/0000000066424-1.jpg")
-        val kitap4 = Book("50 Soruda Evrim",32,"https://i.dr.com.tr/cache/500x400-0/originals/0001896416001-1.jpg")
-        val kitap5 = Book("Tanzimat",56,"https://m.media-amazon.com/images/I/51Uv3M1JWeL._AC_SY1000_.jpg")
-        val kitap6 = Book("Maymunlar Gezegeni",28,"https://img.kitapyurdu.com/v1/getImage/fn:11240119/wh:true/miw:200/mih:200")
+        //binding.rvBookList.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.rvBookList.layoutManager = GridLayoutManager(this,2)//Grid şeklinde hizalama yapıldı
+        binding.rvBookList.adapter = adapter
 
-        booklist.add(kitap1)
-        booklist.add(kitap2)
-        booklist.add(kitap3)
-        booklist.add(kitap4)
-        booklist.add(kitap5)
-        booklist.add(kitap6)
-
-
-
+        adapter.veriyiGetir(booklist)
 
     }
 }
---
+++ app/src/main/java/com/book/bookmarket/adapter/BookListAdapter.kt
@@ -0,0 +1,47 @@
+package com.book.bookmarket.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.book.bookmarket.R
+import com.book.bookmarket.databinding.BookListItemLayoutBinding
+import com.book.bookmarket.model.Book
+import com.bumptech.glide.Glide
+
+class BookListAdapter() :
+    RecyclerView.Adapter<BookListAdapter.BookHolder>() {
+    val booklist: ArrayList<Book> = arrayListOf()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookListAdapter.BookHolder {
+        val binding =
+            BookListItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BookHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BookListAdapter.BookHolder, position: Int) {
+        holder.bind(booklist[position])
+    }
+
+    override fun getItemCount(): Int {
+        return booklist.size
+    }
+
+    inner class BookHolder(private val binding: BookListItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(book: Book) {
+            binding.apply {
+                tvBookName.text = book.bookName
+                tvBookPrice.text = tvBookPrice.context.getString(R.string.book_price, book.bookPrice)
+                Glide.with(imageView.context)
+                    .load(book.bookImage)
+                    .into(imageView)
+            }
+        }
+    }
+
+    fun veriyiGetir(bookListData: ArrayList<Book>) {
+        booklist.clear()
+        booklist.addAll(bookListData)
+        notifyDataSetChanged()
+    }
+}
